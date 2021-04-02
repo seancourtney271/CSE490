17fa9c35410f1394358bb182855168670dee9f03
seancourtney271 <sean.courtney271@gmail.com>
2021-04-02 11:45:47 -0400

    Removed un-necessary flags
    ^

--- project_1/project_1.srcs/sources_1/new/Arithmetic_Logic_Unit_ALU.v
+++ project_1/project_1.srcs/sources_1/new/Arithmetic_Logic_Unit_ALU.v
@@ -1,42 +1,34 @@
 `timescale 1ns / 1ps
 
 //ALU
-module ALU(opcode, op1, op2, result, fCarry, fZero);
+module ALU(opcode, op1, op2, result);
 input [2:0] opcode;
 input [7:0] op1, op2;
 output reg [15:0] result = 16'b0000000000000000;
-output reg [0:0]fCarry, fZero = 1'b0;
     always @(opcode or op1 or op2) 
         begin 
         if(opcode == 000)//ADD
             begin
             result = op1 + op2;
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
             end
         else if(opcode == 001)//SUB
             begin
             result = op1 - op2;
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
             end
         else if(opcode == 010)//NAND
             begin
             result = ~(op1 & op2);
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
             end
         else if(opcode == 011)//NOR
             begin
             result = ~(op1 | op2);
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
             end        
         else if(opcode == 100)//XOR
             begin
             result = op1 ^ op2;
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
+            end
+        else
+            begin
             end            
         end
 endmodule
