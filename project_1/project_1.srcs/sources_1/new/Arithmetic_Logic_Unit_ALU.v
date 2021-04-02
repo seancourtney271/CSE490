ec572747074be5ce0fecfc1dc97616f23b284326
seancourtney271 <sean.courtney271@gmail.com>
2021-04-02 11:42:33 -0400

    Fix ALU
    Fix ALU to orient only needed operations

--- project_1/project_1.srcs/sources_1/new/Arithmetic_Logic_Unit_ALU.v
+++ project_1/project_1.srcs/sources_1/new/Arithmetic_Logic_Unit_ALU.v
@@ -8,49 +8,31 @@ output reg [15:0] result = 16'b0000000000000000;
 output reg [0:0]fCarry, fZero = 1'b0;
     always @(opcode or op1 or op2) 
         begin 
-        if(opcode == 000)
+        if(opcode == 000)//ADD
             begin
             result = op1 + op2;
             fCarry = result[8];
             fZero = (result == 16'b0000000000000000);
             end
-        else if(opcode == 001)
+        else if(opcode == 001)//SUB
             begin
             result = op1 - op2;
             fCarry = result[8];
             fZero = (result == 16'b0000000000000000);
             end
-        else if(opcode == 010)
-            begin
-            result = op1 * op2;
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
-            end
-        else if(opcode == 011)
-            begin
-            result = op1 & op2;
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
-            end
-        else if(opcode == 100)
-            begin
-            result = op1 | op2;
-            fCarry = result[8];
-            fZero = (result == 16'b0000000000000000);
-            end
-        else if(opcode == 101)
+        else if(opcode == 010)//NAND
             begin
             result = ~(op1 & op2);
             fCarry = result[8];
             fZero = (result == 16'b0000000000000000);
             end
-        else if(opcode == 110)
+        else if(opcode == 011)//NOR
             begin
             result = ~(op1 | op2);
             fCarry = result[8];
             fZero = (result == 16'b0000000000000000);
             end        
-        else if(opcode == 111)
+        else if(opcode == 100)//XOR
             begin
             result = op1 ^ op2;
             fCarry = result[8];
