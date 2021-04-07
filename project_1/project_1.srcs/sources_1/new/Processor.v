fd45664b46c93a95079789e799e62932e0484ad0
Alex Kogan <akogan@buffalo.edu>
2021-04-07 18:10:49 -0400

    regfile, control and, alu need finishing, both muxes completable with alu/reg file wires when ready

--- project_1/project_1.srcs/sources_1/new/Processor.v
+++ project_1/project_1.srcs/sources_1/new/Processor.v
@@ -36,13 +36,17 @@ module Processor(
     wire [7:0] Immediate;
     wire [7:0] read_address, write_data, read_data;
     wire data_read_write_signal;
+    wire input_MUX_signal;
+    wire input_MUX_to_ALU;
+    wire output_MUX_signal;
+    wire [7:0] writeback;
     
     
     
     
-    Register_File Registers();
+    Register_File Registers(); // def not done
     
-    Control_Unit(Op_code, Ctrl_op_to_ALU, Ctrl_jump, Ctrl_Mem_Read, Ctrl_Alu_Src, Ctrl_Reg_Write);
+    Control_Unit(Op_code, Ctrl_op_to_ALU, Ctrl_jump, Ctrl_Mem_Read, Ctrl_Alu_Src, Ctrl_Reg_Write); // def not done
     
     Program_Counter PC(clock, Jump_Sig, Jump_Addr, PC_out);
     
@@ -50,15 +54,15 @@ module Processor(
     
     Instruction_Decode ID(Fetched_Instruction, Op_code, ID_out_reg1_bit, ID_out_reg2_bit, ID_out_write_reg, ID_out_Imm_bits, Jump_Addr);
     
-    ALU EX(ALU_op, );
+    ALU EX(ALU_op, ); // def not done
     
     Data_Mem Mem(clock, data_read_write_signal, read_address, write_data/*to mem*/, read_data/*from mem*/);
         
     Sign_Extend_Unit immediate_sign_extend(ID_out_Imm_bits, Immediate);
     
-    MUX2to1 ALU_Input_MUX( , , , );
+    MUX2to1 ALU_Input_MUX( /*reg file data2*/, Immediate, input_MUX_signal, input_MUX_to_ALU); // def not done
     
-    MUX2to1 ALU_Output_MUX( , , , );
+    MUX2to1 ALU_Output_MUX( read_data, /*alu output*/, output_MUX_signal, writeback); // def not done
     
     
 
